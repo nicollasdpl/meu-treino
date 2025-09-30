d7e264ee3f8b38412610260245e33d459f0371d7
nicollasdpl <135170802+nicollasdpl@users.noreply.github.com>
2025-09-30 13:30:08 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'meutreino-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.min.js'
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(r => r ||
+      fetch(e.request).then(res=>{
+        const copy = res.clone();
+        caches.open(CACHE).then(c=>c.put(e.request, copy));
+        return res;
+      }).catch(()=>caches.match('./index.html'))
+    )
+  );
+});
